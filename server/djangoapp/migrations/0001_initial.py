b627677c5ec811d0ba894d1a323359d25ad87931
y0c0ms <majpr@iscte-iul.pt>
2025-07-16 03:58:44 +0100

    Fix all PEP8 linting violations

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -16,7 +16,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True, serialize=False,
+                    verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
             ],
@@ -24,12 +26,26 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarModel',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True, serialize=False,
+                    verbose_name='ID')),
                 ('dealer_id', models.IntegerField()),
                 ('name', models.CharField(max_length=100)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon'), ('HATCHBACK', 'Hatchback'), ('COUPE', 'Coupe'), ('CONVERTIBLE', 'Convertible')], default='SUV', max_length=15)),
-                ('year', models.IntegerField(default=2023, validators=[django.core.validators.MaxValueValidator(2023), django.core.validators.MinValueValidator(2015)])),
-                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='djangoapp.carmake')),
+                ('type', models.CharField(
+                    choices=[
+                        ('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon'),
+                        ('HATCHBACK', 'Hatchback'), ('COUPE', 'Coupe'),
+                        ('CONVERTIBLE', 'Convertible')
+                    ], default='SUV', max_length=15)),
+                ('year', models.IntegerField(
+                    default=2023,
+                    validators=[
+                        django.core.validators.MaxValueValidator(2023),
+                        django.core.validators.MinValueValidator(2015)
+                    ])),
+                ('car_make', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='djangoapp.carmake')),
             ],
         ),
     ]
